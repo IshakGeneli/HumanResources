7ed69d25daec7d75e5c7297b705f4e7c71270ba6
ishakgeneli <s.geneli01@gmail.com>
2022-07-05 08:40:30 +0300

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using HumanResources.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumanResources.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly HumanResourcesDbContext _context;
+
+        public EmployeeController(HumanResourcesDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var employeeList = _context.Employees.ToList();
+            return View(employeeList);
+        }
+    }
+}
