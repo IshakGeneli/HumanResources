41d7d4c237a3240c7306dcfb3c6a01bcb164db19
ishakgeneli <s.geneli01@gmail.com>
2022-08-23 08:46:27 +0300

    employees insert with excel file feature added

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using HumanResources.Contexts;
+﻿using ExcelDataReader;
+using HumanResources.Contexts;
 using HumanResources.GlobalMethods;
 using HumanResources.Identity;
 using HumanResources.Models;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Data;
 
 namespace HumanResources.Controllers
 {
@@ -158,6 +160,109 @@ namespace HumanResources.Controllers
             var jsonResult = JsonConvert.SerializeObject(employee);
 
             return Json(jsonResult);
+        }
+
+        public IActionResult UploadDownloadExcelModal()
+        {
+            return PartialView("_EmployeeExcelPartial");
+        }
+
+        public IActionResult DownloadEmployeeExcelTemplate([FromServices] IWebHostEnvironment hostEnvironment)
+        {
+            var fileName = "toplu-personel-ekle-taslak.xlsx";
+            var filePath = $"{hostEnvironment.WebRootPath}\\files\\excel-files";
+            var memory = DownloadFile(fileName, filePath);
+
+            return File(memory.ToArray(), "application/vnd.ms-excel", fileName);
+        }
+
+        private MemoryStream DownloadFile(string filename, string uploadPath)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), uploadPath, filename);
+            var memory = new MemoryStream();
+
+            if (System.IO.File.Exists(path))
+            {
+                var net = new System.Net.WebClient();
+                var data = net.DownloadData(path);
+                var content = new MemoryStream(data);
+                memory = content;
+            }
+            memory.Position = 0;
+
+            return memory;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> HandleExcelFile(IFormFile excelFile, [FromServices] IWebHostEnvironment hostEnvironment)
+        {
+            string filePath = $"{hostEnvironment.WebRootPath}\\files\\temp-excel-files\\{excelFile.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                excelFile.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+
+            var employees = new List<Employee>();
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var excelReader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var dataset = excelReader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = _ => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true
+                        }
+                    });
+
+                    var table = dataset.Tables;
+
+                    var resultTable = table["Sheet1"];
+
+                    foreach (DataRow row in resultTable.Rows)
+                    {
+                        var a = row[0];
+                        var formatFullName = row[0].ToString().ToLower()
+                            .Replace(" ", "")
+                            .Replace("ç", "c")
+                            .Replace("ğ", "g")
+                            .Replace("ı", "i")
+                            .Replace("ş", "s")
+                            .Replace("ü", "o")
+                            .Replace("ö", "o")
+                            .Replace("ö", "o");
+                        var formatDate = $"{ DateTime.Now.ToString().Substring(0, 10).Replace(".", "").Replace(" ", "")}";
+
+                        var user = new AppUser
+                        {
+                            UserName = $"{formatFullName}_{formatDate}" +
+                                       $"",
+                            Email = $"{formatFullName}@a.com"
+                        };
+                        await _userManager.CreateAsync(user, password: "123456");
+
+                        employees.Add(new Employee()
+                        {
+                            FullName = row[0].ToString(),
+                            Department = row[1].ToString(),
+                            BirthDate = (DateTime)row[2],
+                            HireDate = (DateTime)row[3],
+                            UserId = user.Id,
+                        });
+                    }
+
+                }
+            }
+
+            System.IO.File.Delete(filePath);
+
+            _context.Employees.AddRange(employees);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
